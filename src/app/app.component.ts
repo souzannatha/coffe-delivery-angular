ede4ad0b036f7ebd031d4321202592544fd6dfe7
souzannatha <souzanatha04@gmail.com>
2025-07-09 19:44:34 -0300

    feat: added header component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PrimaryButtonComponent } from './shared/_components/primary-button/primary-button.component';
 import { SecondaryButtonComponent } from './shared/_components/secondary-button/secondary-button.component';
+import { HeaderComponent } from './_components/header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, PrimaryButtonComponent, SecondaryButtonComponent],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
