b9538996fbbe6138309a919436b5fe3b22668649
souzannatha <souzanatha04@gmail.com>
2025-07-09 19:46:40 -0300

    fix: adjusted fold

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PrimaryButtonComponent } from './shared/_components/primary-button/primary-button.component';
 import { SecondaryButtonComponent } from './shared/_components/secondary-button/secondary-button.component';
-import { HeaderComponent } from './_components/header/header.component';
+import { HeaderComponent } from './shared/_components/header/header.component';
 
 @Component({
   selector: 'app-root',
