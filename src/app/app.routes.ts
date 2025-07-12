34bc38866e3c26f80c2f2f2719101f522f3af95e
souzannatha <souzanatha04@gmail.com>
2025-07-12 17:54:00 -0300

    feat: added to checkout and navigation route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
+import { CheckoutComponent } from './features/checkout/checkout.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+  },
+  {
+    path: 'checkout',
+    component: CheckoutComponent,
   },
 ];
--- src/app/shared/_components/coffe-card/coffe-card.component.ts
+++ src/app/shared/_components/coffe-card/coffe-card.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { coffeeCard } from './models/coffee-card';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-coffe-card',
   templateUrl: './coffe-card.component.html',
+  imports: [RouterLink],
   styleUrls: ['./coffe-card.component.scss'],
 })
 export class CoffeCardComponent {
