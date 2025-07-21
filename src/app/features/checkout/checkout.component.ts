1d59ae0ecf0f09c0864e0bd1888239cfe64f255c
souzannatha <souzanatha04@gmail.com>
2025-07-21 19:33:35 -0300

    feat: add coffee select

--- src/app/features/checkout/checkout.component.ts
+++ src/app/features/checkout/checkout.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { InputTextComponent } from '../../shared/_components/input-text/input-text.component';
-import { InputText } from '../../shared/_components/input-text/models/input-text';
 import { CommonModule } from '@angular/common';
 import { SelectPaymentComponent } from '../../shared/_components/select-payment/select-payment.component';
+import { CoffeCartComponent } from '../../shared/_components/coffe-cart/coffe-cart.component';
+import { PrimaryButtonComponent } from '../../shared/_components/primary-button/primary-button.component';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss'],
-  imports: [InputTextComponent, CommonModule, SelectPaymentComponent],
+  imports: [
+    InputTextComponent,
+    CommonModule,
+    SelectPaymentComponent,
+    CoffeCartComponent,
+    PrimaryButtonComponent,
+  ],
 })
 export class CheckoutComponent {
   selectedPayment: string | null = null;
